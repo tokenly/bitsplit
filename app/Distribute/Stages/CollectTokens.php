d1983d0b2eafce45baf1b6eca9c59e6ae8a66144
cryptonaut <nrathman@ironcladtech.ca>
2016-04-14 16:02:19 -0700

    setup CollectTokens stage

--- app/Distribute/Stages/CollectTokens.php
+++ app/Distribute/Stages/CollectTokens.php
@@ -3,7 +3,17 @@ namespace Distribute\Stages;
 
 class CollectTokens extends Stage
 {
-	
-	
-	
+	public function init()
+	{
+		parent::init();
+		//received tokens + fee applied to distro via webhook
+		//do simple check and increment stage
+		$distro = $this->distro;
+		if($distro->asset_received >= $distro->asset_total
+			AND $distro->fee_received >= $distro->fee_total){
+			$distro->incrementStage();
+			return true;		
+		}
+		return false;
+	}
 }
