43b8641d4a769dc1326f4aa650a1d11effabbce0
cryptonaut <nrathman@ironcladtech.ca>
2016-04-29 17:53:42 -0700

    built ConsolidateTxs stage

--- app/Distribute/Stages/ConsolidateTxs.php
+++ app/Distribute/Stages/ConsolidateTxs.php
@@ -1,9 +1,84 @@
 <?php
 namespace Distribute\Stages;
+use Config, UserMeta, DB, Exception, Log, Models\Distribution as Distro, Models\DistributionTx as DistroTx;
 
 class ConsolidateTxs extends Stage
 {
+	public function init()
+	{
+		$distro = $this->distro; 
+		$xchain = xchain();
+		$max_inputs = Config::get('settings.max_tx_outputs');
+		$per_byte = Config::get('settings.miner_satoshi_per_byte');
+		$reasonable_count = 10; //number of inputs this address is allowed before cleanup required
+		$get_utxos = false;
+		
+		$balances = false;
+		try{
+			$balances = $xchain->getAccountBalances($distro->address_uuid, 'default');
+		}
+		catch(Exception $e){
+			Log::error('Error checking balances for distro #'.$distro->id.' consolidation: '.$e->getMessage());
+			return false;
+		}
+		
+		if(!$balances OR !isset($balances['unconfirmed'])){
+			Log::error('Failed getting current balances for distro #'.$distro->id.' consolidation');
+			return false;
+		}		
+		
+		if($balances['unconfirmed']['BTC'] > 0 OR $balances['sending']['BTC'] > 0){
+			Log::info('Waiting for all transactions to confirm for distro #'.$distro->id.' before consolidating');
+			return false;
+		}
+		
+		try{
+			//not actually checking for primes, just getting utxo list
+			$get_utxos = $xchain->checkPrimedUTXOs($distro->address_uuid, 0.0001); 
+		}
+		catch(Exception $e){
+			Log::error('Error checking utxo list for distro #'.$distro->id.' consolidation: '.$e->getMessage());
+			return false;
+		}
+		
+		if(!$get_utxos OR !isset($get_utxos['utxos'])){
+			Log::error('Failed getting utxo list for distro #'.$distro->id.' consolidation');
+			return false;
+		}
+		
+		$get_utxos = $get_utxos['utxos'];
+		$valid_utxos = array();
+		foreach($get_utxos as $utxo){
+			if($utxo['type'] == 'confirmed' AND $utxo['green']){
+				$valid_utxos[] = $utxo;
+			}
+		}
+		$utxo_count = count($valid_utxos);
+		
+		if($utxo_count <= $reasonable_count){
+			Log::info('No more tx consolidation needed for distro #'.$distro->id);
+			$distro->incrementStage();
+			return true;
+		}
 	
-	
-	
+		$cleanup = false;
+		try{
+			$cleanup = $xchain->cleanupUTXOs($distro->address_uuid, 1, $per_byte);	
+		}
+		catch(Exception $e){
+			Log::error('Error consolidating utxos for distro #'.$distro->id.': '.$e->getMessage());
+			return false;
+		}
+		
+		if(!$cleanup OR !isset($cleanup['txid'])){
+			Log::error('Unkown error consolidating utxos for distro #'.$distro->id);
+			return false;
+		}
+		if($cleanup['txid'] == null OR trim($cleanup['txid']) == ''){
+			Log::error('Failed consolidating utxos for distro #'.$distro->id);
+			return false;
+		}
+		Log::info($utxo_count.' UTXOs consolidated for distro #'.$distro->id.' txid: '.$cleanup['txid']);
+		return true;
+	}
 }
