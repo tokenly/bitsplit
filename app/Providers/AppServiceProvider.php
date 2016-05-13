6740f63a4534aea05633c94e5c09074aba51c542
Devon Weller <devon@tokenly.co>
2016-05-12 22:35:09 -0500

    Allow schema override for SSL support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // make all URLs use https
+        if (env('USE_SSL', false)) {
+            URL::forceSchema('https');
+        }
     }
 
     /**
