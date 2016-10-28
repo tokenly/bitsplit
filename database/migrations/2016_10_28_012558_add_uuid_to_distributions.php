c3d5ba11d566349bee8378ab9f31d757d1ce8a79
cryptonaut <nrathman@ironcladtech.ca>
2016-10-27 18:27:37 -0700

    migration for uuid field on distributions table

---
+++ database/migrations/2016_10_28_012558_add_uuid_to_distributions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUuidToDistributions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('distributions', function(Blueprint $table)
+        {
+            $table->char('uuid', 36)->unique();
+            $table->index('uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('distributions', function(Blueprint $table)
+        {
+            $table->dropColumn('uuid');
+        });
+    }
+}
