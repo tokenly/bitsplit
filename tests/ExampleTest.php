0741b3ca3245f61f8d4705cfff8ba6c5d9c1dfea
Devon Weller <devon@tokenly.co>
2017-03-06 21:35:59 -0600

    Fix example test for laravel 5.4 compatibility

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Bitsplit');
+        $this->get('/')
+             ->assertSee('Bitsplit');
     }
 }
