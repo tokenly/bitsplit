83fe8f0e8c305a479d13240f76130ce7e402737d
Ruben B Meseguer <meseguer1998@gmail.com>
2017-06-13 18:10:29 -0400

    DailyFolders' reward token should be ALL or the asset submitted in Distribute form

--- app/Http/Controllers/DistributeController.php
+++ app/Http/Controllers/DistributeController.php
@@ -91,7 +91,11 @@ class DistributeController extends Controller {
         $folding_start_date = date("Y-m-d", strtotime($input['folding_start_date']));
         $folding_end_date = date("Y-m-d", strtotime($input['folding_end_date']));
 		//build address list
-        $folding_address_list = DailyFolder::whereBetween('date', [$folding_start_date, $folding_end_date])->get();
+        $folding_address_list = DailyFolder::whereBetween('date', [$folding_start_date, $folding_end_date])
+                                                        ->where(function ($query) use ($input) {
+                                                            $query->where('reward_token', 'ALL')
+                                                                ->orWhere('reward_token',  $input['asset']);
+                                                        } )->get();
         //TODO: validate that it isn't empty
 
         $total = 0;
