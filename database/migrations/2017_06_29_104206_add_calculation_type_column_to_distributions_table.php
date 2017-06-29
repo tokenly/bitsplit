d03a3547cf021447af3022790224110969e56b3a
Ruben B Meseguer <meseguer1998@gmail.com>
2017-06-29 06:44:56 -0400

    Add calculation_type to distributions table db migration

---
+++ database/migrations/2017_06_29_104206_add_calculation_type_column_to_distributions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCalculationTypeColumnToDistributionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('distributions', function(Blueprint $table)
+        {
+            $table->string('calculation_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('distributions', function(Blueprint $table)
+        {
+            $table->dropColumn(['calculation_type']);
+        });
+    }
+}
