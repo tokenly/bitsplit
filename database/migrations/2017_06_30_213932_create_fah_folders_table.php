e3cba565543d0ee55baef39c85a216e2a7388211
Ruben B Meseguer <meseguer1998@gmail.com>
2017-06-30 17:44:32 -0400

    Create fah_folders table db migration

---
+++ database/migrations/2017_06_30_213932_create_fah_folders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFahFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fah_folders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->bigInteger('total_credit');
+            $table->integer('team');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fah_folders');
+    }
+}
