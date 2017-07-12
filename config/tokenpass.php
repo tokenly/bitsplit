4b89e367be362e9476f07910daf8df3cb7f8aa97
Devon Weller <devon@tokenly.co>
2017-07-12 08:07:00 -0500

    Restored Tokenpass config from env variables

--- config/tokenpass.php
+++ config/tokenpass.php
@@ -3,9 +3,9 @@
 return [
     // Enter your client id and client secret from Tokenpass here
     'client_id'     => env('TOKENPASS_CLIENT_ID'),
-    'client_secret' => 'KTC7eWX1CUGd16Z2Zy8HrVaymDD7IDLZIcRyWE8l',
+    'client_secret' => env('TOKENPASS_CLIENT_SECRET'),
     // this is the URL that Tokenpass uses to redirect the user back to your application
-    'redirect'      => 'http://localhost:8000/account/authorize/callback',
+    'redirect'      => env('SITE_HOST').'/account/authorize/callback',
     // this is the Tokenpass URL
     'base_url'      => rtrim(env('TOKENPASS_PROVIDER_HOST', 'https://tokenpass.tokenly.com'), '/'),
 ];
