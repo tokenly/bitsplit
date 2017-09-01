163b218fb298b9e58793bb2ab495bd93cfd8e9b1
Devon Weller <devon@tokenly.co>
2017-09-01 07:55:55 -0500

    Move stats processes to a separate task schedule

---
+++ app/Console/Commands/FoldingCoinSaveStatsScheduler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Console\Scheduling\ScheduleRunCommand;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Log;
+
+class FoldingCoinSaveStatsScheduler extends ScheduleRunCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fldc-stats-schedule:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs the FoldingCoin stats processes as a separate process';
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function __construct()
+    {
+        // define a custom schedule and skip the normal scheduler
+        $this->schedule = new Schedule();
+        $this->defineSchedule($this->schedule);
+
+        Command::__construct();
+    }
+
+    protected function defineSchedule(Schedule $schedule) {
+        // download the stats
+        $schedule->command('bitsplit:stats')->dailyAt('03:00')->timezone('EST');
+
+        // process the stats
+        $schedule->command('bitsplit:save_stats')->dailyAt('03:00')->timezone('EST');
+    }
+}
