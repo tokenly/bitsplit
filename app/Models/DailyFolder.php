0955866ae6fe6494dcc6a6a592cb10333122915e
Ruben Meseguer <meseguer1998@gmail.com>
2018-02-12 20:22:23 -0300

    Show approximate number of unique daily folders

--- app/Models/DailyFolder.php
+++ app/Models/DailyFolder.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DailyFolder extends Model
 {
     public static function countUniqueFolders()
     {
-        return self::groupBy('bitcoin_address')->count();
+        $result = DB::select("SELECT COUNT(*) FROM (SELECT 1 FROM daily_folders GROUP BY bitcoin_address LIMIT 5000) t;")[0];
+        return get_object_vars($result)['COUNT(*)'] ?? 0;
     }
 }
