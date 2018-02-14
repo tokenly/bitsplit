f88f43d67ebcb2b7bf879eb897ea596a2d81c27f
Devon Weller <devon@tokenly.com>
2018-02-14 07:24:50 -0600

    optimized update code to run in a reasonable time

--- app/Console/Commands/GenerateDailyFolderUUIDs.php
+++ app/Console/Commands/GenerateDailyFolderUUIDs.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\DailyFolder;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class GenerateDailyFolderUUIDs extends Command
 {
@@ -38,12 +40,51 @@ class GenerateDailyFolderUUIDs extends Command
      */
     public function handle()
     {
-        $folders = DailyFolder::where('uuid', null)->get();
-        foreach($folders as $folder){
-            $folder->uuid = md5($folder->team.$folder->username.$folder->bitcoin_address.strtotime(date('Y/m/d', strtotime($folder->date))));
-            $folder->save();
-            $this->info($folder->id.' updated');
+        $total_count = DailyFolder::where('uuid', null)->count();
+        $offset = 0;
+
+        while (true) {
+            $folders = DailyFolder::where('uuid', null)->limit(2500)->get();
+            if ($folders->count() == 0) {
+                break;
+            }
+
+            $uuids = [];
+            foreach($folders as $folder) {
+                $uuid = md5($folder->team.$folder->username.$folder->bitcoin_address.strtotime(date('Y/m/d', strtotime($folder->date))));
+                $uuids[$folder->id] = $uuid;
+
+                ++$offset;
+            }
+
+            $this->bulkUpdateValues(DailyFolder::getModel()->getTable(), $uuids);
+
+            if ($offset % 2500 === 0 or $offset >= $total_count) {
+                $this->info("Updated $offset of $total_count folders");
+            }
         }
-        $this->info('..done');
+
+        $this->info('done');
     }
+
+    protected function bulkUpdateValues($table, array $values)
+    {
+        $cases = [];
+        $ids = [];
+        $params = [];
+
+        foreach ($values as $id => $value) {
+            $id = (int) $id;
+            $cases[] = "WHEN {$id} then ?";
+            $params[] = $value;
+            $ids[] = $id;
+        }
+
+        $ids = implode(',', $ids);
+        $cases = implode(' ', $cases);
+        $params[] = Carbon::now();
+
+        return DB::update("UPDATE `{$table}` SET `uuid` = CASE `id` {$cases} END, `updated_at` = ? WHERE `id` in ({$ids})", $params);
+    }
+
 }
