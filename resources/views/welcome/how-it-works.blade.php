992c5e320906b3745be95b2e82558344791125a3
randolrs <r.shane.randolph@gmail.com>
2018-07-17 10:04:43 -0400

    update how it works text

--- resources/views/welcome/how-it-works.blade.php
+++ resources/views/welcome/how-it-works.blade.php
@@ -9,7 +9,11 @@
         <div class="how-it-works-row">
             <div class="how-it-works-row__panel">
                 <h3 class="step-1">Create an account</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra, nulla a gravida interdum, risus turpis egestas turpis, sit amet convallis elit felis id elit.</p>
+                <p>
+                    <span>Create a free Tokenpass account (powered by</span>
+                    <a href="https://Tokenly.com">Tokenly</a><span>)</span>
+                    <span>using just your email.</span>
+                </p>
             </div>
             <div class="how-it-works-row__panel centered">
                 <img src="{{ asset('img/signup-form.PNG') }}" alt=""/>
@@ -22,7 +26,11 @@
             </div>
             <div class="how-it-works-row__panel">
                 <h3 class="step-2">Add Your Wallet Address</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra, nulla a gravida interdum, risus turpis egestas turpis, sit amet convallis elit felis id elit.</p>
+                <p>
+                    <span>Add a</span>
+                    <a href="https:://counterparty.io">Counterparty</a><span>-compatible</span>
+                    <span>wallet address that holds the tokens you want to distribute.</span>
+                </p>
             </div>
             <div class="how-it-works-row__panel centered mobile-only">
                 <img src="{{ asset('img/tokenpass-add-new-address-form.PNG') }}" alt=""/>
@@ -32,7 +40,7 @@
         <div class="how-it-works-row">
             <div class="how-it-works-row__panel">
                 <h3 class="step-3">Create Your Distribution</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra, nulla a gravida interdum, risus turpis egestas turpis, sit amet convallis elit felis id elit.</p>
+                <p>Specify the name and amount of the token you want to distribute, the methodology to determine who shouldreceive your token, and more.</p>
             </div>
             <div class="how-it-works-row__panel centered">
                 <img src="{{ asset('img/distribution-form.PNG') }}" alt=""/>
@@ -45,7 +53,7 @@
             </div>
             <div class="how-it-works-row__panel">
                 <h3 class="step-4">Review the Details of Your Distribution</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra, nulla a gravida interdum, risus turpis egestas turpis, sit amet convallis elit felis id elit.</p>
+                <p>Review the details of your token distribution, including the number of recipients, total BTC fuel cost, and time/date of completion.</p>
             </div>
             <div class="how-it-works-row__panel centered mobile-only">
                 <img src="{{ asset('img/distribution-details.PNG') }}" alt=""/>
