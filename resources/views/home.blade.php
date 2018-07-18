4c29d7553efa4d4e1811037e327074bfc38a2f7d
randolrs <r.shane.randolph@gmail.com>
2018-07-18 17:55:48 -0400

    remove debug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,6 @@
 				<div class="dashboard__secondary__content">
 					<h2>Admin</h2>
 					<div class="welcome-section-divider"></div>
-					{{ User::needsApprovalCount() }}
 					@if(User::needsApprovalCount())
 						<div>
 						
