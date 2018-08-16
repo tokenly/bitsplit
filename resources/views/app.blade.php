37b1dc301936ca3063574b97a0bf2b018e65a629
Devon Weller <devon@tokenly.com>
2018-08-16 08:21:11 -0500

    remove tawk.to script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,23 +56,6 @@
 		<script src="{{ asset('/js/daterangepicker.js') }}"></script>
 		<script src="{{ asset('/js/jquery.datetimepicker.js') }}"></script>
 		<script src="{{ asset('/js/scripts.js') }}"></script>
-		<!--Start of Tawk.to Script-->
-		<script type="text/javascript">
-			var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-			(function(){
-			var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-			s1.async=true;
-			@if(isset($tawk_override) AND $tawk_override)
-				s1.src='https://embed.tawk.to/{{ $tawk_override }}/default';
-			@else
-				s1.src='https://embed.tawk.to/561c61b6f207135a361e4100/default';
-			@endif
-			s1.charset='UTF-8';
-			s1.setAttribute('crossorigin','*');
-			s0.parentNode.insertBefore(s1,s0);
-			})();
-		</script>
-		<!--End of Tawk.to Script-->	
 		<script src="{{ asset('/js/vue_dev.js') }}"></script>
 		@yield('page_scripts')
 	</body>
