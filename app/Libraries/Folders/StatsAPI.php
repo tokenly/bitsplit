e79859cd352230d8dc417fae928b795074521432
Devon Weller <devon@tokenly.com>
2018-08-28 10:33:09 -0500

    use STATS_PROVIDER_URL variable

--- app/Libraries/Folders/StatsAPI.php
+++ app/Libraries/Folders/StatsAPI.php
@@ -1,26 +1,29 @@
 <?php
 namespace App\Libraries\Folders;
 
-
+use Exception;
 use GuzzleHttp\Client;
 
 class StatsAPI
 {
-
-    const SERVER_URL = 'https://teststatsdownloadapi.azurewebsites.net/';
 
     public function __construct()
     {
 
     }
 
-    function getDistro($start_date, $end_date, $amount) {
-        $client = new Client(['base_uri' => self::SERVER_URL,]);
+    public function getDistro($start_date, $end_date, $amount)
+    {
+        $STATS_PROVIDER_URL = env('STATS_PROVIDER_URL');
+        if (!$STATS_PROVIDER_URL) {
+            throw new Exception("STATS_PROVIDER_URL environment variable not defined", 1);
+        }
+        $client = new Client(['base_uri' => $STATS_PROVIDER_URL]);
         $res = $client->request('GET', '/v1/GetDistro', [
-            'query' => ['startDate' => $start_date, 'endDate' => $end_date, 'amount' => $amount]
+            'query' => ['startDate' => $start_date, 'endDate' => $end_date, 'amount' => $amount],
         ]);
         $response = json_decode($res->getBody(), true);
-        if(empty($response['distro'])) {
+        if (empty($response['distro'])) {
             throw new \Exception('We couldn\'t get the data from the Stats API');
         }
         return $response;
